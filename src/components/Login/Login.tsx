c9ebb271c847a68cceda715a5d51d8611b2d1c1d
Jacquit <jacquit.aja@onja.org>
2021-09-08 08:56:58 +0300

    (Register) Implementing the registration logic

--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -39,7 +39,7 @@ const LoginContainer = styled.div`
   position: relative;
   background-color: #fff;
   ${mediaQueries('lg', null)`
-    padding: 14px 14px 99px 14px;
+    padding: 21px 18px 99px 18px;
   `};
 `
 const Header = styled.div`
